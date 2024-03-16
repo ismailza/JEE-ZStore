1abec44c67e93d235f6c17e0b0062fb07be8ff46
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-16 21:41:52 +0000

    add bo

---
+++ backoffice/src/main/java/ma/fstm/ilisi/zstore/backoffice/bo/Address.java
@@ -0,0 +1,104 @@
+package ma.fstm.ilisi.zstore.backoffice.bo;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "addresses")
+public class Address implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Address() {
+
+    }
+
+    public Address(String street, String city, String state, String postalCode, String country) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    public Address(Long id, String street, String city, String state, String postalCode, String country, Client client) {
+        this.id = id;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    @Override
+    public String toString() {
+        return street + ", " + city + ", " + state + " " + postalCode + ", " + country;
+    }
+}
